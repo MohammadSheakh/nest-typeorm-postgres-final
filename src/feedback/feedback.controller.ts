3dafb75225583783c697cf7c90cfc41b334d5a59
MohammadSheakh <mohammad.sheakh@gmail.com>
2023-09-30 12:03:23 +0600

    running

--- src/feedback/feedback.controller.ts
+++ src/feedback/feedback.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, ParseIntPipe } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { Feedback } from './entity/feedback.entity';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 
+
 @Controller('feedback')
 export class FeedbackController {
   constructor(private feedbackService : FeedbackService){}
+  
+  
+  
   @Get()
   getAllFeedback(){
     return this.feedbackService.getAllFeedback();
@@ -16,4 +20,11 @@ export class FeedbackController {
     console.log(process.env.DB_PASSWORD)
     return this.feedbackService.addFeedback(createFeedbackDto)
   }
+
+  @Get(':id')
+  async findOne(@Param ('id', ParseIntPipe) id: any ):Promise<Feedback>{
+    await console.log("======================================================================================= Heat 1 😢"+id);
+    return this.feedbackService.findOne(id);
+  }
+  
 } 
--- src/feedback/feedback.service.ts
+++ src/feedback/feedback.service.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, ParseIntPipe } from '@nestjs/common';
 import { Feedback } from './entity/feedback.entity';
-import {Repository, InsertResult} from 'typeorm'
+import {Repository, InsertResult, DeleteResult, UpdateResult} from 'typeorm'
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
+
 
 @Injectable()
 export class FeedbackService {
@@ -18,4 +19,35 @@ export class FeedbackService {
   async addFeedback(feedback : CreateFeedbackDto) : Promise<InsertResult>{
     return this .feedbacksRepository.insert(feedback);
   }
+
+  // id : number // type any rakha uchit na 😢
+  async findOne(id: string | number | any):Promise<Feedback | null>{
+    console.log(" Heat 2 😢");
+    if(id != null && id != undefined){
+      return this.feedbacksRepository.findOne({where : {id}});
+    }
+    
+  }
+
+  async update (id : number, feedback: Feedback):Promise<Feedback>{
+    // amra ekhane kichu validation korbo 
+    const feedbackToUpdate = await this.findOne(id);
+    if(feedbackToUpdate == undefined){
+      throw new NotFoundException();
+    }
+    await this.feedbacksRepository.update(id, feedback);
+    return this.findOne(id);
+  }
+
+  async delete (id : number):Promise<DeleteResult>{
+    // amra ekhane kichu validation korbo 
+    const feedbackToDelete = await this.findOne(id);
+    if(feedbackToDelete == undefined){
+      throw new NotFoundException();
+    }
+    return this.feedbacksRepository.delete(id);
+  }
 }
+
+
+
